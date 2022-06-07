c690993e32d2170cf7fdb6f08f966ca7f6f659e8
Claus Matzinger <claus.matzinger+kb@gmail.com>
2022-06-07 21:56:26 +0200

    Project 3 base

--- README.md
+++
@@ -1,2 +0,0 @@
-# building-a-shared-marketplace-with-rust-lp-author
-Repository for liveProject: Building a Shared Marketplace with Rust
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use std::{io, num::ParseIntError};
+mod accounting;
+mod core;
+mod errors;
+mod trading_platform;
+mod tx;
+use crate::{
+    core::{Order, Side},
+    trading_platform::TradingPlatform,
+};
+
+fn read_order_parameters() -> Result<Order, String> {
+    let account = read_from_stdin("Account:");
+    let side = match read_from_stdin("Buy or Sell?:").to_lowercase().as_ref() {
+        "buy" => Ok(Side::Buy),
+        "sell" => Ok(Side::Sell),
+        _ => Err("Unsupported order side"),
+    }?;
+
+    let amount = read_from_stdin("Amount:")
+        .parse()
+        .map_err(|e: ParseIntError| e.to_string())?;
+    let price = read_from_stdin("Price:")
+        .parse()
+        .map_err(|e: ParseIntError| e.to_string())?;
+    Ok(Order {
+        price,
+        amount,
+        side,
+        signer: account,
+    })
+}
+
+fn read_from_stdin(label: &str) -> String {
+    let mut buffer = String::new();
+    println!("{}", label);
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Couldn't read from stdin");
+    buffer.trim().to_owned()
+}
+
+fn main() {
+    println!("Hello, accounting world!");
+
+    let mut ledger = TradingPlatform::new();
+    loop {
+        let input = read_from_stdin(
+            "Choose operation [deposit, withdraw, send, print, txlog, order, orderbook, quit], confirm with return:",
+        );
+        match input.as_str() {
+            "deposit" => {
+                let account = read_from_stdin("Account:");
+
+                let raw_amount = read_from_stdin("Amount:").parse();
+                if let Ok(amount) = raw_amount {
+                    let _ = ledger.deposit(&account, amount);
+                    println!("Deposited {} into account '{}'", amount, account)
+                } else {
+                    eprintln!("Not a number: '{:?}'", raw_amount);
+                }
+            }
+            "withdraw" => {
+                let account = read_from_stdin("Account:");
+                let raw_amount = read_from_stdin("Amount:").parse();
+                if let Ok(amount) = raw_amount {
+                    let _ = ledger.withdraw(&account, amount);
+                } else {
+                    eprintln!("Not a number: '{:?}'", raw_amount);
+                }
+            }
+            "send" => {
+                let sender = read_from_stdin("Sender Account:");
+                let recipient = read_from_stdin("Recipient Account:");
+                let raw_amount = read_from_stdin("Amount:").parse();
+                if let Ok(amount) = raw_amount {
+                    let _ = ledger.send(&sender, &recipient, amount);
+                } else {
+                    eprintln!("Not a number: '{:?}'", raw_amount);
+                }
+            }
+            "order" => match read_order_parameters() {
+                Ok(order) => {
+                    println!("{:?}", ledger.order(order));
+                }
+                Err(msg) => {
+                    eprintln!("Invalid Order parameters: '{:?}'", msg);
+                }
+            },
+            "orderbook" => {
+                println!("The orderbook: {:#?}", ledger.orderbook());
+            }
+            "txlog" => {
+                println!("The TX log: {:#?}", ledger.transactions);
+            }
+            "print" => {
+                println!("The ledger: {:?}", ledger.accounts);
+            }
+            "quit" => {
+                println!("Quitting...");
+                break;
+            }
+            _ => {
+                eprintln!("Invalid option: '{}'", input);
+            }
+        }
+    }
+}
